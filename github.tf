714c6b4f3c7d0d571ef4c20a4bb2e7a05290f3f3
Rushikesh Deshmukh <121441809+rushi-21776@users.noreply.github.com>
2024-01-08 02:36:16 +0530

    Update github.tf

--- github.tf
+++ github.tf
@@ -1,5 +1,1 @@
-resource "github_repository" "name" {
-    name = "Hosting-Websit-on-AWS-S3-using-Terraform"
-    description = "This repo is created using terraform"
-    visibility = "public"  
-}
+
